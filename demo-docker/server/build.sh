e426e041c67117f86d194faf74aaed14173b2eb1
zrbcool <zhangrongbincool@163.com>
2019-08-26 17:04:05 +0800

    pepper metrics demo

--- demo-docker/server/build.sh
+++ demo-docker/server/build.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
+CUR_DIR=`pwd`
+PRJ_ROOT="../../"
+cd ${PRJ_ROOT}/demo-app
+mvn clean package -DskipTests
+cd ${CUR_DIR}
+rm -f *.jar
+cp ${PRJ_ROOT}/demo-app/demo-server/target/*.jar .
 COMPONENT="pepper-metrics-demo-server"
-docker build -t zrbcool/${COMPONENT}:latest -f Dockerfile .
+sudo docker build -t zrbcool/${COMPONENT}:latest -f Dockerfile .
